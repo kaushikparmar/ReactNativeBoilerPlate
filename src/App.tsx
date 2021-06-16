f32c458a4e69bb9636dc84369aff85786acee213
kaushikparmar <kaushik.kumar.2189@gmail.com>
2021-06-16 21:37:13 +0530

    Added Image

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,16 @@ import {
 import store from './redux/store/rootStore';
 import { Provider } from 'react-redux';
 import { User } from './views/User';
-
+import { View, Image } from 'react-native';
 const App = () => {
 
   return (
     <Provider store={store}>
       <SafeAreaView>
         <StatusBar />
+        <View style={{backgroundColor:'#ddd'}}>
+        <Image source={require('./assets/Logo.png')} style={{height:50, width:'100%'}} />
+        </View>
         <User />
       </SafeAreaView>
     </Provider>
